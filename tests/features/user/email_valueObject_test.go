302c199243d4e4d30bbea17ea55ed8b98200c501
Qwikle <qwikle.ziko@gmail.com>
2023-10-02 23:57:03 +0200

    add test to check empty value

--- tests/features/user/email_valueObject_test.go
+++ tests/features/user/email_valueObject_test.go
@@ -13,3 +13,21 @@ func TestValidEmail(t *testing.T) {
 		t.Fatalf("ValidEmail(%q) = %q, want %q", want, got, want)
 	}
 }
+
+func TestInvalidEmail(t *testing.T) {
+	want := "qwikle@gmail"
+
+	_, err := user.ValidEmail(want)
+	if err == nil {
+		t.Fatalf("ValidEmail(%q) = %q, want %q", want, err, want)
+	}
+}
+
+func TestEmptyEmail(t *testing.T) {
+	want := ""
+
+	_, err := user.ValidEmail(want)
+	if err == nil {
+		t.Fatalf("ValidEmail(%q) = %q, want %q", want, err, want)
+	}
+}
