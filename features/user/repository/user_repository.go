87829b518055ef042f5f1dcf876b052e552a4c18
Qwikle <qwikle.ziko@gmail.com>
2023-10-03 00:43:14 +0200

    set only interface to follow Clean archi

---
+++ features/user/repository/user_repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"touchedFlowed/features/user/entities"
+	"touchedFlowed/features/user/requests"
+)
+
+type Repository interface {
+	CreateUser(user *requests.CreateUserRequest) (*entities.User, error)
+	GetUserByEmail(email string) (*entities.User, error)
+	GetUserById(id uint64) (*entities.User, error)
+	UpdateUser(user *entities.User) (*entities.User, error)
+	DeleteUser(id uint64) error
+}
