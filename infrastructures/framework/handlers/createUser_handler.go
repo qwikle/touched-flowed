360763df124c5332a506bdacd5c7b37acf93d0ec
Qwikle <qwikle.ziko@gmail.com>
2023-10-02 23:56:16 +0200

    add condition to check the content length of the body

--- infrastructures/framework/handlers/createUser_handler.go
+++ infrastructures/framework/handlers/createUser_handler.go
@@ -9,13 +9,19 @@ import (
 
 func CreateUser(c *gin.Context) {
 	var newUser user.CreateUserRequest
+
+	if c.Request.ContentLength == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Request body is empty"})
+		return
+	}
+
 	if err := c.ShouldBindJSON(&newUser); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	response, err := user.NewCreateUserUseCase(user.NewRepository(utils.NewDatabase())).Execute(&newUser)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusCreated, response)
