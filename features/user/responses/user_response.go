8a73c91debe67f560963b56dc9c2cc6ecef6e0fb
Qwikle <qwikle.ziko@gmail.com>
2023-10-03 00:57:07 +0200

    create fromEntity function

--- features/user/responses/user_response.go
+++ features/user/responses/user_response.go
@@ -1,8 +1,17 @@
 package responses
 
+import "touchedFlowed/features/user/entities"
+
 type CreateUserResponse struct {
 	Id        uint64 `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
+}
+
+func (r *CreateUserResponse) FromEntity(user *entities.User) {
+	r.Id = user.Id
+	r.FirstName = user.FirstName
+	r.LastName = user.LastName
+	r.Email = user.Email
 }
