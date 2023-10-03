f1032230190b76fdd88cb528c4cdeb363cca3848
Qwikle <qwikle.ziko@gmail.com>
2023-10-03 20:23:09 +0200

    refactor: set only used function and create generic row I

--- features/utils/database_repository.go
+++ features/utils/database_repository.go
@@ -1,67 +1,10 @@
 package utils
 
-import (
-	"database/sql"
-	_ "github.com/lib/pq"
-	"os"
-)
+type Rows interface {
+	Scan(dest ...interface{}) error
+	Next() bool
+}
 
 type Database interface {
-	Query(query string, args ...interface{}) (*sql.Rows, error)
-	Exec(query string, args ...interface{}) (sql.Result, error)
-	QueryRow(query string, args ...interface{}) *sql.Row
-	QueryTx(tx *sql.Tx, query string, args ...interface{}) (*sql.Rows, error)
-	ExecTx(tx *sql.Tx, query string, args ...interface{}) (sql.Result, error)
-	QueryRowTx(tx *sql.Tx, query string, args ...interface{}) *sql.Row
-}
-
-type database struct {
-	db *sql.DB
-}
-
-func connect() (*sql.DB, error) {
-	db, err := sql.Open(os.Getenv("DATABASE_TYPE"), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-
-func (d database) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	return d.db.Query(query, args...)
-}
-
-func (d database) Exec(query string, args ...interface{}) (sql.Result, error) {
-	return d.db.Exec(query, args...)
-}
-
-func (d database) QueryRow(query string, args ...interface{}) *sql.Row {
-	return d.db.QueryRow(query, args...)
-}
-
-func (d database) QueryTx(tx *sql.Tx, query string, args ...interface{}) (*sql.Rows, error) {
-	return tx.Query(query, args...)
-}
-
-func (d database) ExecTx(tx *sql.Tx, query string, args ...interface{}) (sql.Result, error) {
-	return tx.Exec(query, args...)
-}
-
-func (d database) QueryRowTx(tx *sql.Tx, query string, args ...interface{}) *sql.Row {
-	return tx.QueryRow(query, args...)
-}
-
-var DatabaseInstance Database
-
-func NewDatabase() Database {
-	if DatabaseInstance == nil {
-		db, err := connect()
-		if err != nil {
-			panic(err)
-		}
-		DatabaseInstance = &database{
-			db: db,
-		}
-	}
-	return DatabaseInstance
+	Query(query string, args ...interface{}) (Rows, error)
 }
