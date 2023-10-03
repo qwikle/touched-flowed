81fe89a9167aba4e21f542aa69cbc0a619404451
Qwikle <qwikle.ziko@gmail.com>
2023-10-03 20:27:07 +0200

    feat: add getAll function

--- features/user/repository/user_repository.go
+++ features/user/repository/user_repository.go
@@ -11,4 +11,5 @@ type Repository interface {
 	GetUserById(id uint64) (*entities.User, error)
 	UpdateUser(user *entities.User) (*entities.User, error)
 	DeleteUser(id uint64) error
+	GetAll() ([]*entities.User, error)
 }
