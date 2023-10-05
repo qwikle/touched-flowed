5dbcb59e42cd6105c58ea99e57b93000c72ff110
Qwikle <qwikle.ziko@gmail.com>
2023-10-06 00:10:36 +0200

    feat: add init and WIP logout handler

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"touchedFlowed/infrastructures"
+)
 import "touchedFlowed/infrastructures/framework/handlers"
 import "github.com/joho/godotenv"
 
@@ -9,9 +12,12 @@ func main() {
 	if err1 != nil {
 		return
 	}
+	infrastructures.Init()
 	r := gin.Default()
-	r.POST("/users", handlers.CreateUser)
-	r.POST("/users/login", handlers.SignInUser)
+	r.POST("/sign-up", handlers.CreateUser)
+	r.POST("/sign-in", handlers.SignInUser)
+	//todo: Finish sign-out
+	//r.DELETE("/sign-out", middlewares.AuthMiddleware(), handlers.SignOutUser)
 	err := r.Run()
 	if err != nil {
 		return
