04f2626b13b5c090aed669eb5a2c7d298fa673f0
Qwikle <qwikle.ziko@gmail.com>
2023-11-10 21:35:47 +0100

    refactor: add onDisconnect and onConnect func change the handleWs

--- infrastructures/framework/websocket/server.go
+++ infrastructures/framework/websocket/server.go
@@ -3,24 +3,31 @@ package websocket
 import (
 	"encoding/json"
 	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
+	"sync"
 )
 
 type SocketServer interface {
-	HandleWS(writer http.ResponseWriter, request *http.Request) (*Socket, error)
+	HandleWS(writer http.ResponseWriter, request *http.Request) error
 	Emit(roomName string, data string, socket *Socket)
 	Broadcast(event string, data string)
 	AddSocket(s *Socket)
 	RemoveSocket(s *Socket)
 	AddSocketToRoom(roomName string, s *Socket)
-	CreateRoom(roomName string)
+	CreateRoom(roomName string) *Room
 	GetRoom(roomName string) *Room
 	RemoveSocketFromRoom(roomName string, s *Socket)
-	Listen()
+	On(event string, callback SocketEventCallback)
+	OnConnect(callback Callback)
+	OnDisconnect(callback Callback)
+	Start()
 	Close()
 }
 
-func eventToString(event string, data string) (string, error) {
+type Callback func(*Socket)
+
+func stringify(event string, data string) (string, error) {
 	var socketMessage SocketMessage
 	socketMessage.Event = event
 	socketMessage.Payload = data
@@ -31,7 +38,7 @@ func eventToString(event string, data string) (string, error) {
 	return string(message), nil
 }
 
-func stringToEvent(message string) (SocketMessage, error) {
+func parse(message string) (SocketMessage, error) {
 	var socketMessage SocketMessage
 	err := json.Unmarshal([]byte(message), &socketMessage)
 	if err != nil {
@@ -48,102 +55,163 @@ var upgrade = websocket.Upgrader{
 }
 
 type socketServer struct {
-	rooms          map[string]*Room
-	sockets        map[string]*Socket
-	socketsChannel chan *Socket
+	rooms             sync.Map
+	sockets           sync.Map
+	connectedSockets  chan *Socket
+	leavedSockets     chan *Socket
+	socketChannel     chan *Socket
+	events            sync.Map
+	mu                sync.Mutex
+	onConnect         Callback
+	onConnectIsSet    bool
+	onDisconnect      Callback
+	onDisconnectIsSet bool
 }
 
-func (ss *socketServer) Listen() {
+func (ss *socketServer) On(event string, callback SocketEventCallback) {
+	_, ok := ss.events.Load(event)
+	if !ok {
+		ss.events.Store(event, callback)
+	}
+}
+
+func (ss *socketServer) Start() {
 	for {
 		select {
-		case socket := <-ss.socketsChannel:
+		case socket := <-ss.connectedSockets:
+			if ss.onConnect != nil {
+				ss.onConnect(socket)
+			}
+			ss.socketChannel <- socket
+		case socket := <-ss.leavedSockets:
+			if ss.onDisconnect != nil {
+				ss.onDisconnect(socket)
+			}
+		case socket := <-ss.socketChannel:
+			go func() {
+				ss.events.Range(func(key, value interface{}) bool {
+					event := key.(string)
+					callback := value.(SocketEventCallback)
+					socket.On(event, callback)
+					return true
+				})
+			}()
 			socket.Listen()
 		}
 	}
 }
 
-func (ss *socketServer) HandleWS(writer http.ResponseWriter, request *http.Request) (*Socket, error) {
+func (ss *socketServer) HandleWS(writer http.ResponseWriter, request *http.Request) error {
 	ws, err := upgrade.Upgrade(writer, request, nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	s := NewSocket(ws, request.RemoteAddr)
 	ss.AddSocket(s)
-	return s, nil
+	return nil
 }
 
 func (ss *socketServer) AddSocket(s *Socket) {
-	ss.sockets[s.Id] = s
-	ss.socketsChannel <- s
+	ss.sockets.Store(s.Id, s)
+	ss.connectedSockets <- s
+}
+
+func (ss *socketServer) OnConnect(callback Callback) {
+	if ss.onConnectIsSet {
+		log.Panic("OnConnect callback can be set only once")
+		return
+	}
+	ss.onConnect = callback
+}
+
+func (ss *socketServer) OnDisconnect(callback Callback) {
+	if ss.onDisconnectIsSet {
+		log.Panic("OnDisconnect callback can be set only once")
+		return
+	}
+	ss.onDisconnect = callback
 }
 
 func (ss *socketServer) RemoveSocket(s *Socket) {
-	for _, room := range ss.rooms {
-		delete(room.Sockets, s.Id)
-	}
-	delete(ss.sockets, s.Id)
+	ss.rooms.Range(func(key, value interface{}) bool {
+		room := value.(*Room)
+		room.RemoveSocket(s)
+		return true
+	})
+
+	ss.sockets.Delete(s.Id)
+	ss.leavedSockets <- s
 }
 
 func (ss *socketServer) AddSocketToRoom(roomName string, s *Socket) {
 	room := ss.GetRoom(roomName)
 	if room == nil {
-		ss.CreateRoom(roomName)
-		room = ss.GetRoom(roomName)
+		room = ss.CreateRoom(roomName)
 	}
-	room.Sockets[s.Id] = s
+	room.AddSocket(s)
 }
 
 func (ss *socketServer) RemoveSocketFromRoom(roomName string, s *Socket) {
-	if room, o := ss.rooms[roomName]; o {
-		delete(room.Sockets, s.Id)
-		message, err := eventToString("leave", s.Id)
-		if err != nil {
-			return
-		}
-		ss.Emit(roomName, message, nil)
+	room := ss.GetRoom(roomName)
+	if room == nil {
+		return
 	}
+	room.RemoveSocket(s)
 }
 
 func (ss *socketServer) Emit(roomName string, data string, s *Socket) {
-	socketMessage, err := stringToEvent(data)
+	socketMessage, err := parse(data)
 	if err != nil {
 		return
 	}
 	room := ss.GetRoom(roomName)
 	if room == nil {
+		log.Panicf("Room %s does not exist", roomName)
 		return
 	}
-	for _, socket := range room.Sockets {
-		if s == nil || socket.Id != s.Id {
-			socket.Emit(socketMessage.Event, socketMessage.Payload)
+	room.Sockets.Range(func(key, value interface{}) bool {
+		socket := value.(*Socket)
+		if socket.Id != s.Id {
+			socket.Emit(socketMessage.Event, data)
 		}
-	}
+		return true
+	})
 }
 
 func (ss *socketServer) Broadcast(event string, data string) {
-	message, err := eventToString(event, data)
+	message, err := stringify(event, data)
 	if err != nil {
 		return
 	}
-	for _, socket := range ss.sockets {
+	ss.sockets.Range(func(key, value interface{}) bool {
+		socket := value.(*Socket)
 		socket.Emit(event, message)
-	}
+		return true
+	})
 }
 
-func (ss *socketServer) CreateRoom(roomName string) {
+func (ss *socketServer) CreateRoom(roomName string) *Room {
 	if room := ss.GetRoom(roomName); room == nil {
-		ss.rooms[roomName] = NewRoom(roomName)
+		ss.rooms.Store(roomName, NewRoom(roomName))
 	}
+	room, _ := ss.rooms.Load(roomName)
+	return room.(*Room)
 }
 
 func (ss *socketServer) GetRoom(roomName string) *Room {
-	return ss.rooms[roomName]
+	room, ok := ss.rooms.Load(roomName)
+	if !ok {
+		return nil
+	}
+	return room.(*Room)
 }
 
 func (ss *socketServer) Close() {
-	for _, socket := range ss.sockets {
+	ss.sockets.Range(func(key, value interface{}) bool {
+		socket := value.(*Socket)
 		socket.Close()
-	}
+		return true
+	})
 }
 
 func GetInstance() SocketServer {
@@ -156,9 +224,9 @@ func GetInstance() SocketServer {
 func newSocketServer() SocketServer {
 	if socketServerInstance == nil {
 		socketServerInstance = &socketServer{
-			rooms:          make(map[string]*Room),
-			sockets:        make(map[string]*Socket),
-			socketsChannel: make(chan *Socket, 100),
+			connectedSockets: make(chan *Socket, 100),
+			leavedSockets:    make(chan *Socket, 100),
+			socketChannel:    make(chan *Socket, 100),
 		}
 	}
 	return socketServerInstance
