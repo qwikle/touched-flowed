da8a0f5c87172465b8480e6c7b3085e59b581e35
Qwikle <qwikle.ziko@gmail.com>
2023-11-10 21:37:10 +0100

    refactor: use sync.Map instead of map and add func Length

--- infrastructures/framework/websocket/room.go
+++ infrastructures/framework/websocket/room.go
@@ -1,21 +1,32 @@
 package websocket
 
+import (
+	"sync"
+	"sync/atomic"
+)
+
 type Room struct {
 	Name    string
-	Sockets map[string]*Socket
+	Sockets sync.Map
+	count   uint64
 }
 
 func (r *Room) AddSocket(s *Socket) {
-	r.Sockets[s.Id] = s
+	r.Sockets.Store(s.Id, s)
+	atomic.AddUint64(&r.count, 1)
 }
 
 func (r *Room) RemoveSocket(s *Socket) {
-	delete(r.Sockets, s.Id)
+	r.Sockets.Delete(s.Id)
+	atomic.AddUint64(&r.count, ^uint64(0))
 }
 
 func NewRoom(name string) *Room {
 	return &Room{
-		Name:    name,
-		Sockets: make(map[string]*Socket),
+		Name: name,
 	}
+}
+
+func (r *Room) Length() uint64 {
+	return atomic.LoadUint64(&r.count)
 }
